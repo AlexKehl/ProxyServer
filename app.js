b929389d8ed6ffe21d3fade26b5b69b634f2ccbe
Alexander Kehl <alex.kehl.32@gmail.com>
2024-03-25 20:49:05 +0100

    add secret

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const Unblocker = require('unblocker');
+const express = require("express");
+const Unblocker = require("unblocker");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  console.error("Please create a .env file with the PORT variable");
+  process.exit(1);
+}
+
+if (!process.env.SECRET) {
+  console.error("Please create a .env file with the PORT variable");
+  process.exit(1);
+}
 
 const app = express();
 
-const unblocker = new Unblocker({prefix: '/proxy/'});
+const unblocker = new Unblocker({
+  prefix: "/proxy/",
+  requestMiddleware: [
+    (req) => {
+      if (req?.headers?.secret !== process.env.SECRET) {
+        throw new Error();
+      }
+    },
+  ],
+});
 app.use(unblocker);
 
-app.listen(process.env.PORT || 8080).on('upgrade', unblocker.onUpgrade);
-console.log("Node Unblocker Server Running On Port:", process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080).on("upgrade", unblocker.onUpgrade);
+console.log("Node Unblocker Server Running On Port:", process.env.PORT || 8080);
